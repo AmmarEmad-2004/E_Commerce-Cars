e336fd594dd735ba8d1e2ba5e85739afdd2cb9a0
mariam ibrahim <neamamohammed649@gmail.com>
2025-08-07 14:50:00 +0300

    create counter cubit & state for cart screen

---
+++ lib/modules/home/presentation/manager/cubit/counter_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterInitial());
+  double totalPrice = 120;
+  int count = 1;
+  void increment(double price) {
+    count++;
+    totalPrice += price;
+    emit(CounterSuccess(totalPrice: totalPrice, count: count));
+  }
+
+  void decrement(double price) {
+    if (count <= 0) return;
+    count--;
+    totalPrice -= price;
+    emit(CounterSuccess(totalPrice: totalPrice, count: count));
+  }
+
+  double getTotalPrice() {
+    return totalPrice;
+  }
+}
