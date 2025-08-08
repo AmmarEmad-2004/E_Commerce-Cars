195cc635ffcac60221b0c23d21df653c9bc8611e
AmmarEmad-2004 <za0662003@email.com>
2025-08-08 18:28:52 +0300

    finish add cars screen

--- lib/modules/add_car/presentation/widgets/add_car_form.dart
+++ lib/modules/add_car/presentation/widgets/add_car_form.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:cars_app/core/helpers/add_car_validation.dart';
+import 'package:cars_app/core/helpers/snack_bar.dart';
 import 'package:cars_app/core/themes/app_colors.dart';
 import 'package:cars_app/core/widgets/custom_background.dart';
 import 'package:cars_app/core/widgets/custom_text_feild.dart';
@@ -16,10 +18,33 @@ class AddCarForm extends StatefulWidget {
 }
 
 class _AddCarFormState extends State<AddCarForm> {
+  final _formKey = GlobalKey<FormState>();
   final nameController = TextEditingController();
   final priceController = TextEditingController();
   final descController = TextEditingController();
   File? imageFile;
+
+  void clearControllers() {
+    nameController.clear();
+    priceController.clear();
+    descController.clear();
+    setState(() {
+      imageFile = null;
+    });
+  }
+
+  // ✅ تعمل dispose لما الصفحة تتقفل
+  void disposeControllers() {
+    nameController.dispose();
+    priceController.dispose();
+    descController.dispose();
+  }
+
+  @override
+  void dispose() {
+    disposeControllers();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,95 +53,113 @@ class _AddCarFormState extends State<AddCarForm> {
       child: CustomBackground(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  'Car Details',
-                  style: TextStyle(
-                    color: AppColors.primaryColor,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Car Details',
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
-              ),
-              CustomSize(h: 20),
-              Text(
-                'Car Name',
-                style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
-              ),
-              CustomTextFormField(
-                hint: 'e.g., Tesla Model 3',
-                controller: nameController,
-              ),
-              CustomSize(h: 8),
-              Text(
-                'Price',
-                style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
-              ),
-              CustomTextFormField(
-                hint: r'e.g., $45,000',
-                controller: priceController,
-                keyboardType: TextInputType.number,
-              ),
-              CustomSize(h: 8),
-              Text(
-                'Description',
-                style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
-              ),
-              CustomTextFormField(
-                maxLines: 4,
-                hint: 'Enter a detailed description of the car...',
-                controller: descController,
-              ),
-              CustomSize(h: 16),
-              ImageUpLoadedBox(
-                onImageSelected: (file) {
-                  setState(() {
-                    imageFile = file;
-                  });
-                },
-                imageFile: imageFile,
-              ),
-              CustomSize(h: 16),
-              BlocConsumer<AddCarCubit, AddCarState>(
-                listener: (context, state) {
-                  if (state is AddCarSuccess) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Car Added Successfully")),
-                    );
-                    Navigator.pop(context);
-                  } else if (state is AddCarError) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(state.message)),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is AddCarLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  return ElevatedButton(
-                    onPressed: () {
-                      if (imageFile != null) {
-                        context.read<AddCarCubit>().addCar(
+                CustomSize(h: 20),
+                Text(
+                  'Car Name',
+                  style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
+                ),
+                CustomTextFormField(
+                  validator:
+                      (p0) => AddCarValidations.validateCarName(
+                        context,
+                        nameController.text,
+                      ),
+                  hint: 'e.g., Tesla Model 3',
+                  controller: nameController,
+                ),
+                CustomSize(h: 8),
+                Text(
+                  'Price',
+                  style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
+                ),
+                CustomTextFormField(
+                  validator:
+                      (p0) => AddCarValidations.validatePrice(
+                        context,
+                        priceController.text,
+                      ),
+                  hint: r'e.g., $45,000',
+                  controller: priceController,
+                  keyboardType: TextInputType.number,
+                ),
+                CustomSize(h: 8),
+                Text(
+                  'Description',
+                  style: TextStyle(color: AppColors.primaryColor, fontSize: 16),
+                ),
+                CustomTextFormField(
+                  validator:
+                      (p0) => AddCarValidations.validateDescription(
+                        context,
+                        descController.text,
+                      ),
+                  maxLines: 4,
+                  hint: 'Enter a detailed description of the car...',
+                  controller: descController,
+                ),
+                CustomSize(h: 16),
+                ImageUpLoadedBox(
+                  onImageSelected: (file) {
+                    setState(() {
+                      imageFile = file;
+                    });
+                  },
+                  imageFile: imageFile,
+                ),
+                CustomSize(h: 16),
+                BlocConsumer<AddCarCubit, AddCarState>(
+                  listener: (context, state) {
+                    if (state is AddCarSuccess) {
+                      ShowSnackBar.show(context, "Car Added Successfully");
+                      clearControllers();
+                    } else if (state is AddCarError) {
+                      ShowSnackBar.show(context, state.message);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is AddCarLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          if (imageFile != null) {
+                            context.read<AddCarCubit>().addCar(
                               name: nameController.text,
                               description: descController.text,
                               price: double.tryParse(priceController.text) ?? 0,
                               imagePath: imageFile!.path,
                             );
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Please select an image")),
-                        );
-                      }
-                    },
-                    child: const Text("Add Car"),
-                  );
-                },
-              )
-            ],
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Please select an image"),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      child: const Text("Add Car"),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
